df7b66b681b7bde2783b2eb27008d34bf35bea98
Danielbyte <danielmasango7@gmail.com>
2021-10-29 00:48:32 +0200

    Interface for DDT bombs

--- game-source-code/DDTBombsController.h
+++ game-source-code/DDTBombsController.h
@@ -1,1 +1,15 @@
+#ifndef DDTBOMBSCONTROLLER_H
+#define DDTBOMBSCONTROLLER_H
 
+#include <ctime>
+
+class DDTBombsController
+{
+public:
+    DDTBombs();
+    bool getIfCanCreateBomb() const;
+    void setIfCanCreateBomb();
+private:
+    bool canCreateBomb;
+};
+#endif // DDTBOMBSCONTROLLER_H
