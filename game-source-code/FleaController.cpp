7f2d2ce3e2d8adb3e17550d85aaf04e2cf2c6888
Danielbyte <danielmasango7@gmail.com>
2021-11-01 22:06:22 +0200

    Interface and implementation framework for flea controller

--- game-source-code/FleaController.cpp
+++ game-source-code/FleaController.cpp
@@ -1,3 +1,19 @@
 #include "FleaController.h"
 
-FleaController::FleaController(){};
+FleaController::FleaController() {};
+
+void FleaController::update_flea(vector<shared_ptr<Flea>>& flea_object, vector<shared_ptr<Sprite>>& flea_sprite,
+                                 shared_ptr<MushroomFieldController>& mushField)
+{
+
+}
+
+void FleaController::spawn_mushroom(const float& x, const float& y, shared_ptr<MushroomFieldController>& mushField)
+{
+
+}
+
+bool FleaController::set_if_can_spawn_flea(shared_ptr<MushroomFieldController>& mushField)
+{
+
+}
--- game-source-code/FleaController.h
+++ game-source-code/FleaController.h
@@ -1,10 +1,19 @@
 #ifndef FLEACONTROLLER_H
 #define FLEACONTROLLER_H
+
+#include "Flea.h"
+#include "GameDataType.h"
+#include "SfmlDataType.h"
+#include "mushroomFieldController.h"
+
 class FleaController
 {
 private:
     FleaController();
+    void update_flea(vector<shared_ptr<Flea>>&,vector<shared_ptr<Sprite>>&,shared_ptr<MushroomFieldController>&);
 public:
+    void spawn_mushroom(const float&, const float&, shared_ptr<MushroomFieldController>&);
+    bool set_if_can_spawn_flea(shared_ptr<MushroomFieldController>&);
 
 };
 #endif // FLEACONTROLLER_H
