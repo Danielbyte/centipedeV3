dd186158e9ddb3f6122870622814ff5aaad9c5b6
Danielbyte <danielmasango7@gmail.com>
2021-11-05 00:41:57 +0200

    Test is bullet is erased on collision with flea

--- test-source-code/Tests4.cpp
+++ test-source-code/Tests4.cpp
@@ -49,6 +49,49 @@ TEST_CASE("Test if collision is detected between bullet and flea")
     CHECK(isCollided == true);
 }
 
+TEST_CASE("Test if bullet is erased on collision with flea")
+{
+    auto logic = Logic{};
+    auto col = Collision{};
+    Texture flea_texture;
+    vector<shared_ptr<Sprite>>bullet_sprite;
+    vector<shared_ptr<Sprite>>flea_sprite_vector;
+
+    //create bullet;
+    logic.create_bullet(bullet_sprite);
+
+    //create flea
+    auto flea_sprite = std::make_shared<Sprite>(Sprite());
+    auto pos = logic.create_flea();
+    if(!flea_texture.loadFromFile("resources/flea1.png")) throw CouldNotLoadPicture{};
+    flea_sprite ->setTexture(flea_texture);
+    flea_sprite -> setOrigin(vector2f(0.f,0.f));
+    flea_sprite -> setPosition(pos);
+    flea_sprite_vector.push_back(flea_sprite);
+
+    //update the flea twice
+    logic.update_flea(flea_sprite_vector);
+    logic.update_flea(flea_sprite_vector);
+
+    //get reference to the flea position
+    auto flea_iter = flea_sprite_vector.begin();
+    pos = (*flea_iter) -> getPosition();
+
+    //give this position to the bullet so that they collisde
+    auto bullet_iter = bullet_sprite.begin();
+    (*bullet_iter) -> setPosition(pos);
+    auto bulletPos = (*bullet_iter) -> getPosition();
+
+    auto size = bullet_sprite.size();
+    //One bullet present
+    CHECK(size == 1);
+    logic.collision_between_bullet_and_flea(bullet_sprite, flea_sprite_vector);
+
+    //after collision, there should be no bullet
+    size = bullet_sprite.size();
+    CHECK(size == 0);
+}
+
 
 /*
 TEST_CASE("Centipede changes direction when encountered walls"){}
