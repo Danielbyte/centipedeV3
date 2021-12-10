fac5b255fe46f2a95acd277067657d008ce62073
Danielbyte <danielmasango7@gmail.com>
2021-12-11 00:22:16 +0200

    cradle to death anime

--- game-source-code/SpriteAnimation.h
+++ game-source-code/SpriteAnimation.h
@@ -4,16 +4,15 @@
 class SpriteAnimation : public Sprite //inherits from Sprite class in sfml library
 {
 public:
-    //SpriteAnimation(){};
-
-    //initialization list
-    SpriteAnimation(Texture& texture, int frameCountWidth, int frameCountHeight, float framesPerSecond = 60,
-                    int initialFrame = 0, int lastFrame = 0, bool isAnimated = true, bool isLooped = true);
-
-    ~SpriteAnimation(){};
-
-
+    SpriteAnimation();
+    Texture death_texture1;
+    Texture death_texture2;
+    Texture death_texture3;
+    Texture death_texture4;
+    Texture death_texture5;
+    Texture death_texture6;
 private:
+    //vector <shared_ptr<Sprite>>
 
 };
 #endif // SPRITEANIMATION_H
