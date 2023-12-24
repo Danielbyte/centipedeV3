d37d5ab27c325fa9c5d94595294a42d2f0149a97
Danielbyte <danielmasango7@gmail.com>
2023-12-24 15:33:17 +0200

    Renamed SpriteAnimation class to SoundManager class

---
+++ game-source-code/soundManager.h
@@ -0,0 +1,14 @@
+#ifndef SOUNDMANAGER_H
+#define SOUNDMANAGER_H
+#include "gameResources.h"
+
+class SoundManager
+{
+public:
+    SoundManager();
+
+private:
+    //vector <shared_ptr<Sprite>>
+
+};
+#endif // SPRITEANIMATION_H
